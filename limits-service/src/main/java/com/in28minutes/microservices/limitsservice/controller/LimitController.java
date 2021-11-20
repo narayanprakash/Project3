603c6cc9fd837de3a20392e6ac9475f717d15149
Prakash Narayan <prakashnarayan001@gmail.com>
2021-11-20 16:59:26 +0530

    Changing Mapping

--- limits-service/src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitController.java
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitController.java
@@ -2,6 +2,7 @@ package com.in28minutes.microservices.limitsservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.microservices.limitsservice.bean.Limit;
@@ -12,7 +13,7 @@ public class LimitController {
 	@Autowired
 	private Configuration configuration;
 
-	@GetMapping("/limits")
+	@PostMapping("/limits")
 	public Limit retrieveLimits() {
 		return new Limit(configuration.getMinimum(), configuration.getMaximum());
 	}
