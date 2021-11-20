a01105d97deec5bd6ddde2a4d27fbe5bfd59a45f
Prakash Narayan <prakashnarayan001@gmail.com>
2021-11-20 18:08:10 +0530

    message

--- src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitController.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.microservices.limitsservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.in28minutes.microservices.limitsservice.bean.Limit;
 import com.in28minutes.microservices.limitsservice.configuration.Configuration;
 
-@RestController
+@Controller
 public class LimitController {
 	@Autowired
 	private Configuration configuration;
