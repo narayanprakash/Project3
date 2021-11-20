839320ca06de556e3b684a4c79246e0e11b41806
Prakash Narayan <prakashnarayan001@gmail.com>
2021-11-20 17:04:14 +0530

    resolved merge conflict

--- limits-service/src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitController.java
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitController.java
@@ -12,8 +12,8 @@ import com.in28minutes.microservices.limitsservice.configuration.Configuration;
 public class LimitController {
 	@Autowired
 	private Configuration configuration;
-
-	@PostMapping("/limits")
+	
+	@PostMapping("/api/limits")
 	public Limit retrieveLimits() {
 		return new Limit(configuration.getMinimum(), configuration.getMaximum());
 	}
